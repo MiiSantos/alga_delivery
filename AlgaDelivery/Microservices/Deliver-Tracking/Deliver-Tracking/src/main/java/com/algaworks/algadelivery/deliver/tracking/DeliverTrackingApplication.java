32fd560f46f6826142535e56bdc4f8c49ac42bee
C1330268 Miria Andressa Silva Santos <c1330268@interno.bb.com.br>
2025-07-22 14:47:05 -0300

    initial project

---
+++ AlgaDelivery/Microservices/Deliver-Tracking/Deliver-Tracking/src/main/java/com/algaworks/algadelivery/deliver/tracking/DeliverTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algadelivery.deliver.tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliverTrackingApplication.class, args);
+	}
+
+}
